c2ec0c3dcc073e61d278879689ca3a5f3a169702
Yan-Long-Huang <leo277482@gmail.com>
2024-07-05 16:00:02 +0800

    update HC-SR04.c

--- STM32/Application_Library/Best_System_Robot/Source/HC-SR04.c
+++ STM32/Application_Library/Best_System_Robot/Source/HC-SR04.c
@@ -9,54 +9,54 @@ HC_SR04_TypeDef hc_sr04[3] = {
 
 
 
-// 發送 Trig 信號並回傳距離
+// Send Trig signal and return distance
 float HC_SR04_GetDistance(HC_SR04_TypeDef* sensor)
 {
     uint32_t startTick = HAL_GetTick();
 
-    // 送出 Trig 信號
+    // Send Trig signal
     HAL_GPIO_WritePin(sensor->trig_port, sensor->trig_pin, GPIO_PIN_SET);
     delay_us(10);
     HAL_GPIO_WritePin(sensor->trig_port, sensor->trig_pin, GPIO_PIN_RESET);
 
-    // 等待回波信號檢測完成，或者超過0.1秒鐘
+    // Wait for echo signal to be detected
     while (!sensor->pulseDetected) {
+        // Return an error value or appropriate value to indicate timeout
         if ((HAL_GetTick() - startTick) >= 100) {
-            // 超時後的處理
-            sensor->distance = -1; // 返回一個錯誤值或者合適的值表示超時
+            sensor->distance = -1;
             break;
         }
     }
-    // 重置脈衝檢測標誌
+    // Reset the pulse detection flag
     sensor->pulseDetected = 0;
     return sensor->distance;
 }
 
 
-// 處理 HC-SR04 中斷
+// Handle HC-SR04 interrupt
 void HC_SR04_HandleInterrupt(uint16_t GPIO_Pin, HC_SR04_TypeDef* sensors, int num_sensors)
 {
     for (int i = 0; i < num_sensors; i++) {
         HC_SR04_TypeDef* sensor = &sensors[i];
         if (GPIO_Pin == sensor->echo_pin) {
             if (HAL_GPIO_ReadPin(sensor->echo_port, sensor->echo_pin) == GPIO_PIN_SET) {
-                // 脈衝開始
+                // Pulse start
                 sensor->pulseStart = __HAL_TIM_GET_COUNTER(&htim6);
-                sensor->overflow_count = 0; // 重置溢出計數
+                sensor->overflow_count = 0; // Reset overflow count
             } else {
-                // 脈衝結束
+                // Pulse end
                 sensor->pulseEnd = __HAL_TIM_GET_COUNTER(&htim6);
                 uint32_t echo_duration;
 
                 if (sensor->pulseEnd < sensor->pulseStart) {
-                    // 處理計數器溢出
+                    // Handle counter overflow
                     echo_duration = (0xFFFF - sensor->pulseStart) + sensor->pulseEnd + (sensor->overflow_count * 0xFFFF);
                 } else {
                     echo_duration = sensor->pulseEnd - sensor->pulseStart;
                 }
 
                 sensor->distance = (echo_duration * 0.03432) / 2;
-                sensor->pulseDetected = 1; // 設置脈衝檢測標誌
+                sensor->pulseDetected = 1; // Set pulse detection flag
             }
         }
     }
